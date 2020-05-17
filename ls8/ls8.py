393f6f8e9f9dc24b5f490a6f732495cd0165e2ea
sweyome <sweyome1@gmail.com>
2020-05-17 19:49:06 -0400

    CPU and test files copied

---
+++ ls8/ls8.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""Main."""
+
+import sys
+from cpu import *
+
+cpu = CPU()
+
+if len(sys.argv) != 2:
+    print("ERROR: must have file name")
+    sys.exit(1)
+cpu.load(sys.argv[1])
+cpu.trace()
+cpu.run()
+
+
+# 
